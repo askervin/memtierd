9512f789ee4da8b901311d658065c4c9e876c8b4
Antti Kervinen <antti.kervinen@intel.com>
2025-10-10 13:57:33 +0300

    fixmeup: fix patchRuntimeConfig

--- test/e2e/affinity.test-suite/k8s/s8c4k/test01-balloons/code.var.sh
+++ test/e2e/affinity.test-suite/k8s/s8c4k/test01-balloons/code.var.sh
@@ -41,7 +41,7 @@ vm-put-file $(instantiate balloons.conf) balloons.conf
 if vm-command "helm ls | grep nri-resource-policy-balloons"; then
     vm-command "helm uninstall nri-resource-policy-balloons"
 fi
-vm-install-helm-pkg nri-plugins/nri-resource-policy-balloons --values balloons.conf
+vm-install-helm-pkg nri-plugins/nri-resource-policy-balloons --values balloons.conf --set patchRuntimeConfig=true
 
 CPUREQ="500m" CPULIM="" MEMREQ=50M MEMLIM=""
 ANN0="balloon.balloons.resource-policy.nri.io/container.pod0c0: pkg0"
